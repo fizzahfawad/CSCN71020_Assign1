08d50e459933aed1b85e29d3ce00d69e24e44476
Fizzah Fawad <Ffawad0875@conestogac.on.ca>
2023-09-29 12:25:15 -0400

    Adjusting and Adding

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -1,25 +1,33 @@
+
 #include <stdio.h>
 
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
 
 	printWelcomeMenu();
-
+	
 	printOptions();
 
 	int inputNum;
 
 	printf("Enter operation number: ");
 	scanf_s("%1o", &inputNum);
+	scanf_s("%2o", &inputNum);
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
+	}
+	switch (inputNum)
+	{
+	case 2:
+		subtract();
 	}
 
 }
@@ -45,3 +53,12 @@ void add() {
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
 }
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
+	}
